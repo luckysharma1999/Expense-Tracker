be48178c1bb17adc2d0e68ad25b9c65652e2c20f
Lucky <luckysharma2708@gmail.com>
2023-02-01 03:10:54 +0530

    first commit

---
+++ src/components/Expenses/ExpensesList.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+import ExpenseItem from './ExpenseItem';
+import './ExpensesList.css';
+
+const ExpensesList = (props) => {
+  if (props.items.length === 0) {
+    return <h2 className='expenses-list__fallback'>Found no expenses.</h2>;
+  }
+  const onDelete = (id) => {
+    console.log(id);
+  };
+  
+
+  return (
+    <ul className='expenses-list'>
+      {props.items.map((expense) => (
+        <ExpenseItem
+          onDelete={onDelete}
+          key={expense.id}
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
+        />
+      ))}
+    </ul>
+  );
+};
+
+export default ExpensesList;
